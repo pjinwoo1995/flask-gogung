8760a5939cafad55c3c77657ed20774c291ca293
pjinwoo1995 <59362971+pjinwoo1995@users.noreply.github.com>
2020-12-16 21:45:59 +0900

    project change

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -5,36 +5,4 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def main():
-    return render_template('main.html')
-
-@bp.route('/01')
-def page1():
-    return render_template('page1.html')
-
-@bp.route('/02')
-def page2():
-    return render_template('page2.html')
-
-@bp.route('/03')
-def page3():
-    return render_template('page3.html')
-
-@bp.route('/04')
-def page4():
-    return render_template('page4.html')
-
-@bp.route('/05')
-def page5():
-    return render_template('page5.html')
-
-@bp.route('/06')
-def page6():
-    return render_template('page6.html')
-
-@bp.route('/07')
-def page7():
-    return render_template('page7.html')
-
-@bp.route('/08')
-def page8():
-    return render_template('page8.html')
+    return render_template('FBXLoaderTest.html')
